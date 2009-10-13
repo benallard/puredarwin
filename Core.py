1fb1000fca99bc4bdf5b23619b7b6c88d0167e83
Benoît Allard <benoit.allard@gmx.de>
2009-10-13 23:21:01 +0200

    rename parameters

--- Core.py
+++ Core.py
@@ -22,16 +22,14 @@ def RAM(dout, din, addr, we, clk, rst_n, width, depth):
 
     return read, write
 
-def Core(addr, clk, CORESIZE):
+def Core(addr, clk, rst_n, maxSize):
     """ Our Core """
 
-    AddrWidth = len(intbv(min=0, max=CORESIZE))
-
-    OpCode = RAM(opcode_out, opcode_in, opcode_addr, opcode_we, clk, 5, CORESIZE)
-    Modif = RAM(modif_out, modif_in, modif_addr, modif_we, clk, 3, CORESIZE)
-    AMode = RAM(amode_out, amode_in, amode_addr, amode_we, clk, 3, CORESIZE)
-    ANumber = RAM(number_out, anumber_in, anumber_addr, anumber_we, clk, AddrWidth, CORESIZE)
-    BMode = RAM(bmode_out, bmode_in, bmode_addr, bmode_we, clk, 3, CORESIZE)
-    BNumber = RAM(bnumber_out, bnumber_in, bnumber_addr, bnumber_we, clk, AddrWidth, CORESIZE)
+    OpCode = RAM(opcode_out, opcode_in, opcode_addr, opcode_we, clk, 5, maxSize)
+    Modif = RAM(modif_out, modif_in, modif_addr, modif_we, clk, 3, maxSize)
+    AMode = RAM(amode_out, amode_in, amode_addr, amode_we, clk, 3, maxSize)
+    ANumber = RAM(number_out, anumber_in, anumber_addr, anumber_we, clk, MARSparam.AddrWidth, maxSize)
+    BMode = RAM(bmode_out, bmode_in, bmode_addr, bmode_we, clk, 3, maxSize)
+    BNumber = RAM(bnumber_out, bnumber_in, bnumber_addr, bnumber_we, clk, MARSparam.AddrWidth, maxSize)
     
     return OpCode, Modif, AMode, ANumber, BMode, BNumber
