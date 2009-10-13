7e0a8a728768752598315ee5ed591a7da5461e88
Benoît Allard <benoit.allard@gmx.de>
2009-10-14 00:20:32 +0200

    Update the Core

--- Core.py
+++ Core.py
@@ -6,6 +6,8 @@ This is the central memory storage for the Warriors, the "Hill"
 
 from myhdl import *
 
+import MARSparam
+
 def RAM(dout, din, addr, we, clk, rst_n, width, depth):
     """ Basic RAM model """
     
@@ -22,14 +24,49 @@ def RAM(dout, din, addr, we, clk, rst_n, width, depth):
 
     return read, write
 
-def Core(addr, clk, rst_n, maxSize):
+def Core(addr, din, dout, we, clk, rst_n, maxSize):
     """ Our Core """
 
-    OpCode = RAM(opcode_out, opcode_in, opcode_addr, opcode_we, clk, 5, maxSize)
-    Modif = RAM(modif_out, modif_in, modif_addr, modif_we, clk, 3, maxSize)
-    AMode = RAM(amode_out, amode_in, amode_addr, amode_we, clk, 3, maxSize)
-    ANumber = RAM(number_out, anumber_in, anumber_addr, anumber_we, clk, MARSparam.AddrWidth, maxSize)
-    BMode = RAM(bmode_out, bmode_in, bmode_addr, bmode_we, clk, 3, maxSize)
-    BNumber = RAM(bnumber_out, bnumber_in, bnumber_addr, bnumber_we, clk, MARSparam.AddrWidth, maxSize)
-    
-    return OpCode, Modif, AMode, ANumber, BMode, BNumber
+    # Constant for the Size of an Instruction
+    # usefull for slicing
+    InstrWidth = 14 + 2 * MARSparam.AddrWidth
+
+    # Those are VHDL Aliases
+    opcode_in = din[InstrWidth:InstrWidth-5]
+    dout[InstrWidth:InstrWidth-5] = opcode_out
+
+    modif_in = din[InstrWidth-5:InstrWidth-8]
+    dout[InstrWidth-5:InstrWidth-8] = modif_out
+
+    amode_in = din[InstrWidth-8:InstrWidth-11]
+    dout[InstrWidth-8:InstrWidth-11] = amode_out
+
+    anumber_in = din[InstrWidth-11:InstrWidth-11-MARSparam.AddrWidth]
+    dout[InstrWidth-11:InstrWidth-11-MARSparam.AddrWidth] = anumber_out
+
+    bmode_in = din[MARSparam.AddrWidth+3:MARSparam.AddrWidth]
+    dout[MARSparam.AddrWidth+3:MARSparam.AddrWidth] = bmode_out
+
+    bnumber_in = din[MARSparam.AddrWidth:0]
+    dout[MARSparam.AddrWidth:0] = bnumber_out
+
+    opcode_we = we[6:5]
+    modif_we = we[5:4]
+    amode_we = we[4:3]
+    anumber_we = we[3:2]
+    bmode_we = we[2:1]
+    bnumber_we = we[1:0]
+
+    # My RAMs
+    OpCode = RAM(opcode_out, opcode_in, addr, opcode_we, clk, rst_n, 5, maxSize)
+    Modif = RAM(modif_out, modif_in, addr, modif_we, clk, rst_n, 3, maxSize)
+    AMode = RAM(amode_out, amode_in, addr, amode_we, clk, rst_n, 3, maxSize)
+    ANumber = RAM(number_out, anumber_in, addr, anumber_we, clk, rst_n, MARSparam.AddrWidth, maxSize)
+    BMode = RAM(bmode_out, bmode_in, addr, bmode_we, clk, rst_n, 3, maxSize)
+    BNumber = RAM(bnumber_out, bnumber_in, addr, bnumber_we, clk, rst_n, MARSparam.AddrWidth, maxSize)
+
+    @always_comb
+    def comb():
+        pass
+
+    return OpCode, Modif, AMode, ANumber, BMode, BNumber, comb
--- testTaskQueue.py
+++ testTaskQueue.py
@@ -9,6 +9,7 @@ from Task import TaskQueue
 class testTaskQueueProperties(TestCase):
 
     def testMapping(self):
+        """ Global internal mapping """
 
         def test(w, ipin, ipout, re, we, empty, clk, rst_n):
             rst_n.next = False
@@ -16,6 +17,7 @@ class testTaskQueueProperties(TestCase):
             rst_n.next = True
             yield delay(1)
 
+            # empty at init ?
             for i in range (2):
                 clk.next = False
                 yield delay(10)
@@ -23,7 +25,7 @@ class testTaskQueueProperties(TestCase):
                 clk.next = True
                 yield delay(10)
                 self.assertEqual(empty, (True, True)[i])
-            print "begin"
+
             for i in range(10):
                 print "(%d : %d)" % (i, w)
                 clk.next = False
@@ -51,9 +53,10 @@ class testTaskQueueProperties(TestCase):
         empty_i = Signal(bool())
         clk_i = Signal(bool())
         rst_n_i = Signal(bool())
+
         dut = TaskQueue(Warrior_i, IPin_i, IPout_i, re_i, we_i, empty_i, clk_i, rst_n_i, 2)
         check = test(Warrior_i, IPin_i, IPout_i,re_i, we_i, empty_i, clk_i, rst_n_i )
-        # traceSignals(test, dut)
+
         sim = Simulation(dut, check)
         sim.run(quiet = 1)
 
