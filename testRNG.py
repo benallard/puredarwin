6ab79b5ee5fafd99fcad440638aa7e05a3a4bce1
Benoît Allard <benoit.allard@gmx.de>
2009-11-10 23:25:37 +0100

    Add a second RNG test

--- testRNG.py
+++ testRNG.py
@@ -47,5 +47,48 @@ class testRNG(TestCase):
         sim = Simulation(dut, check)
         sim.run(quiet=1)
 
+    def testPeriod(self):
+        """
+        Check the Period of the RNG
+        20 x CORESIZE is a good one ...
+        """
+
+
+        def test(out, clk, rst_n):
+
+            mem = {}
+
+            rst_n.next = True
+            yield delay(3)
+            rst_n.next = False
+            yield delay(2)
+            rst_n.next = True
+
+            index = 0
+            while True:
+                index+=1
+                if index % 4000 == 0:
+                    print index
+                clk.next = False
+                yield delay(5)
+                clk.next = True
+                yield delay(5)
+                try:
+                    mem[int(out)] += 1
+                except KeyError:
+                    mem[int(out)] = 1
+                self.assertEquals(mem[int(out)], 1, index)
+                if index > 20 * 8000:
+                    raise StopSimulation
+
+        out_i = Signal(intbv()[20:])
+        clk_i, rst_n_i = [Signal(bool()) for i in range(2)]
+
+        dut = RNG(out_i, clk_i, rst_n_i)
+        check = test(out_i, clk_i, rst_n_i)
+
+        sim = Simulation(dut, check)
+        sim.run(quiet=1)
+
 if __name__ == "__main__":
     unittest.main()
