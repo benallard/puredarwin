d73a0ed8a744b3ad09c6d3ec27abd1ccf2105e14
Benoît Allard <benoit.allard@gmx.de>
2009-11-10 20:42:12 +0100

    Add a Serial Rx module (+ testbench)

---
+++ Loader.py
@@ -0,0 +1,104 @@
+"""
+
+Ok, let's load the warriors from a serial line,
+
+and generate the Random position through a LFSR
+
+"""
+
+from myhdl import *
+
+t_Parity = enum("EVEN", "ODD", "MARK", "SPACE", "NO")
+
+def Rx(Rx, data, clk, ack, nbBits, baudrate, parity, clkrate):
+    """
+    My serial line
+    """
+
+    t_State = enum("WAIT", "DELAY", "RECEIVE")
+    sampling_time =  int(clkrate / baudrate)
+
+    state = Signal(t_State.WAIT)
+
+    data_i = Signal(intbv(0)[nbBits:])
+
+    bitReceived = Signal(intbv(0, min=0, max=nbBits+3))
+    time = Signal(intbv(0,min=0, max=sampling_time+2))
+
+    parity_bit = Signal(bool())
+    parity_ok = Signal(bool())
+
+
+    @always(clk.posedge)
+    def rx():
+        if state == t_State.WAIT:
+            parity_ok.next = False
+            if Rx == False:
+                state.next = t_State.DELAY
+                bitReceived.next = 0
+                time.next = sampling_time // 2
+                if parity == t_Parity.EVEN:
+                    parity_bit.next = False
+                elif parity == t_Parity.ODD:
+                    parity_bit.next = True
+        elif state == t_State.DELAY:
+            time.next = time + 1
+            if time == sampling_time:
+                time.next = 0
+                state.next = t_State.RECEIVE
+        elif state == t_State.RECEIVE:
+            ack.next = False
+            time.next = time + 1
+            if time == sampling_time:
+                time.next = 0
+                bitReceived.next = bitReceived + 1
+                print "sampling %d: %d" % (bitReceived, Rx)
+                if bitReceived == nbBits: # parity bit
+                    if parity == t_Parity.NO:
+                        state.next = t_State.WAIT
+                        if Rx: # stop bit
+                            ack.next = True
+                            data.next = data_i
+                            data_i.next = 0
+                        else:
+                            ack.next = False
+                    elif parity == t_Parity.MARK:
+                        if Rx:
+                            parity_ok.next = True
+                        else:
+                            parity_ok.next = False
+                    elif parity == t_Parity.SPACE:
+                        if Rx:
+                            parity_ok.next = False
+                        else:
+                            parity_ok.next = True
+                    else:
+                        if parity_bit == Rx:
+                            parity_ok.next = True
+                        else:
+                            parity_ok.next = False
+                        
+                elif  bitReceived == nbBits + 1: # stop bit
+                    if parity == t_Parity.NO:
+                        raise Error("Shouldn't be there")
+                    state.next = t_State.WAIT
+                    if Rx:
+                        ack.next = parity_ok
+                        data.next = data_i
+                        data_i.next = 0
+                    else:
+                        ack.next = False
+                else: # usual communication
+                    parity_bit.next = parity_bit ^ Rx 
+                    data_i.next = int(Rx) * (2**(nbBits-1)) +  data_i[nbBits:1]
+#                    data_i.next = concat(intbv(Rx.val)[1:], data_i.val[nbBits-1:])
+            
+
+    return rx
+
+def RNG(out, clk, rst_n):
+    """ My LFSR """
+    pass
+
+def Loader(Rx, we, Dout, we1, IP1, clk, rst_n, req, ack, baudrate):
+    pass
---
+++ testRx.py
@@ -0,0 +1,113 @@
+from myhdl import *
+
+from random import randrange
+
+from Loader import Rx, t_Parity
+
+import unittest
+from unittest import TestCase
+
+class testRx(TestCase):
+    """ test our Serial Receiver """
+
+
+    def testNoParity(self):
+        """ Without parity bit """
+
+        clkPeriod = 10 # ns
+
+        period = int(1e9 / 9600000)
+
+        def clkDrv(clk):
+            while True:
+                yield delay(clkPeriod//2)
+                clk.next = not clk
+
+        def test(Tx, data, ack, nbBits):
+            Tx.next = True
+            for i in range (20):
+                yield delay(randrange(70))
+                totransmit = intbv(val=randrange(2**nbBits))
+                print "transmitting %d" % totransmit
+                for i in range(nbBits+2):
+                    yield delay(period // 2)
+                    if i == 0:
+                        Tx.next = False
+                    elif i == nbBits+1:
+                        print "stop !"
+                        Tx.next = True
+                    else:
+                        Tx.next = totransmit[(i-1)]
+                    print "setting %d: %d" % (i, Tx.next)
+                    yield delay(period // 2)
+                yield delay(period)
+                self.assertEquals(ack, True)
+                self.assertEquals(data, totransmit)
+            raise StopSimulation
+                    
+        Rx_i, ack_i, clk_i = [Signal(bool()) for i in range(3)]
+        data_i = Signal(intbv()[4:])
+
+        dut = Rx(Rx=Rx_i, data=data_i, clk = clk_i, ack = ack_i, nbBits=4, baudrate=9600000, parity=t_Parity.NO, clkrate=1e9/clkPeriod)
+        check = test(Rx_i, data_i, ack_i, 4)
+        clock = clkDrv(clk_i)
+        
+        sim = Simulation(dut, check, clock)
+        sim.run(quiet=1)
+
+
+
+    def testOddParity(self):
+        """ With Odd parity bit """
+
+        clkPeriod = 10 # ns
+
+        period = int(1e9 / 9600)
+
+        def clkDrv(clk):
+            while True:
+                yield delay(clkPeriod//2)
+                clk.next = not clk
+
+        def test(Tx, data, ack, nbBits):
+            Tx.next = True
+            for i in range (20):
+                yield delay(randrange(70))
+                parity = True
+                totransmit = intbv(val=randrange(2**nbBits))
+                for i in range (nbBits):
+                    if totransmit[i]:
+                        parity = not parity
+                print "transmitting %d" % totransmit
+                for i in range(nbBits+3):
+                    yield delay(period // 2)
+                    if i == 0:
+                        Tx.next = False
+                    elif i == nbBits+1:
+                        print "parity"
+                        Tx.next = parity
+                    elif i == nbBits+2:
+                        print "stop !"
+                        Tx.next = True
+                    else:
+                        Tx.next = totransmit[(i-1)]
+                    print "setting %d: %d" % (i, Tx.next)
+                    yield delay(period // 2)
+                yield delay(period)
+                self.assertEquals(ack, True)
+                self.assertEquals(data, totransmit)
+            raise StopSimulation
+                    
+        Rx_i, ack_i, clk_i = [Signal(bool()) for i in range(3)]
+        data_i = Signal(intbv()[7:])
+
+        dut = Rx(Rx=Rx_i, data=data_i, clk = clk_i, ack = ack_i, nbBits=7, baudrate=9600, parity=t_Parity.ODD, clkrate=1e9/clkPeriod)
+        check = test(Rx_i, data_i, ack_i, 7)
+        clock = clkDrv(clk_i)
+        
+        sim = Simulation(dut, check, clock)
+        sim.run(quiet=1)
+
+if __name__ == "__main__":
+    unittest.main()
+        
